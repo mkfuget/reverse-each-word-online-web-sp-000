187d2489b30d9e11a337ae212859a0958b095dd0
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 08:18:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,3 @@
 def reverse_each_word(text)
-  index = 0;
-  array = text.split(" ");
-  text.split(" ").each do |x|
-    array[index] = x.reverse;
-    index+=1
-  end
-  return array.join(" ")
+  return text.split(" ").collect { |x| x.reverse}.join(" ")
 end
